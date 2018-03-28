02a22f184143346e7add5a92833a86a4d8881296
wang ying <wang.ying@sap.com>
2018-03-28 11:34:32 +0800

    GUI almost done

--- ConTool/ConTool/Form1.Designer.cs
+++ ConTool/ConTool/Form1.Designer.cs
@@ -28,20 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
             this.tableMain = new System.Windows.Forms.TableLayoutPanel();
             this.txtOriginFileLocation = new System.Windows.Forms.TextBox();
             this.btnRead = new System.Windows.Forms.Button();
             this.txtTargetFileLocation = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnTargetFile = new System.Windows.Forms.Button();
             this.radioDMSbackend = new System.Windows.Forms.RadioButton();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.btnConvert = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
+            this.flowParameters = new System.Windows.Forms.FlowLayoutPanel();
+            this.labelMaxCol = new System.Windows.Forms.Label();
+            this.numMaxCol = new System.Windows.Forms.NumericUpDown();
+            this.labelMaxRow = new System.Windows.Forms.Label();
+            this.numMaxRow = new System.Windows.Forms.NumericUpDown();
+            this.lblSeperator = new System.Windows.Forms.Label();
+            this.txtSeperator = new System.Windows.Forms.TextBox();
+            this.progressConverting = new System.Windows.Forms.ProgressBar();
             this.openExcelFile = new System.Windows.Forms.OpenFileDialog();
             this.saveTargetFile = new System.Windows.Forms.SaveFileDialog();
+            this.timerProgress = new System.Windows.Forms.Timer(this.components);
             this.tableMain.SuspendLayout();
             this.flowLayoutPanel1.SuspendLayout();
+            this.flowParameters.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numMaxCol)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMaxRow)).BeginInit();
             this.SuspendLayout();
             // 
             // tableMain
@@ -50,15 +63,18 @@
             this.tableMain.Controls.Add(this.txtOriginFileLocation, 0, 0);
             this.tableMain.Controls.Add(this.btnRead, 1, 0);
             this.tableMain.Controls.Add(this.txtTargetFileLocation, 0, 1);
-            this.tableMain.Controls.Add(this.button2, 1, 1);
-            this.tableMain.Controls.Add(this.radioDMSbackend, 0, 2);
-            this.tableMain.Controls.Add(this.flowLayoutPanel1, 1, 2);
+            this.tableMain.Controls.Add(this.btnTargetFile, 1, 1);
+            this.tableMain.Controls.Add(this.radioDMSbackend, 0, 4);
+            this.tableMain.Controls.Add(this.flowLayoutPanel1, 1, 4);
+            this.tableMain.Controls.Add(this.flowParameters, 0, 2);
+            this.tableMain.Controls.Add(this.progressConverting, 0, 3);
             this.tableMain.Name = "tableMain";
             // 
             // txtOriginFileLocation
             // 
             resources.ApplyResources(this.txtOriginFileLocation, "txtOriginFileLocation");
             this.txtOriginFileLocation.Name = "txtOriginFileLocation";
+            this.txtOriginFileLocation.TextChanged += new System.EventHandler(this.FileNames_changed);
             // 
             // btnRead
             // 
@@ -71,12 +87,14 @@
             // 
             resources.ApplyResources(this.txtTargetFileLocation, "txtTargetFileLocation");
             this.txtTargetFileLocation.Name = "txtTargetFileLocation";
+            this.txtTargetFileLocation.TextChanged += new System.EventHandler(this.FileNames_changed);
             // 
-            // button2
+            // btnTargetFile
             // 
-            resources.ApplyResources(this.button2, "button2");
-            this.button2.Name = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.btnTargetFile, "btnTargetFile");
+            this.btnTargetFile.Name = "btnTargetFile";
+            this.btnTargetFile.UseVisualStyleBackColor = true;
+            this.btnTargetFile.Click += new System.EventHandler(this.btnTargetFile_Click);
             // 
             // radioDMSbackend
             // 
@@ -98,6 +116,7 @@
             resources.ApplyResources(this.btnConvert, "btnConvert");
             this.btnConvert.Name = "btnConvert";
             this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
             // 
             // btnCancel
             // 
@@ -105,6 +124,75 @@
             resources.ApplyResources(this.btnCancel, "btnCancel");
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // flowParameters
+            // 
+            this.tableMain.SetColumnSpan(this.flowParameters, 2);
+            this.flowParameters.Controls.Add(this.labelMaxCol);
+            this.flowParameters.Controls.Add(this.numMaxCol);
+            this.flowParameters.Controls.Add(this.labelMaxRow);
+            this.flowParameters.Controls.Add(this.numMaxRow);
+            this.flowParameters.Controls.Add(this.lblSeperator);
+            this.flowParameters.Controls.Add(this.txtSeperator);
+            resources.ApplyResources(this.flowParameters, "flowParameters");
+            this.flowParameters.Name = "flowParameters";
+            // 
+            // labelMaxCol
+            // 
+            resources.ApplyResources(this.labelMaxCol, "labelMaxCol");
+            this.labelMaxCol.Name = "labelMaxCol";
+            // 
+            // numMaxCol
+            // 
+            resources.ApplyResources(this.numMaxCol, "numMaxCol");
+            this.numMaxCol.Maximum = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this.numMaxCol.Name = "numMaxCol";
+            this.numMaxCol.Value = new decimal(new int[] {
+            107,
+            0,
+            0,
+            0});
+            // 
+            // labelMaxRow
+            // 
+            resources.ApplyResources(this.labelMaxRow, "labelMaxRow");
+            this.labelMaxRow.Name = "labelMaxRow";
+            // 
+            // numMaxRow
+            // 
+            resources.ApplyResources(this.numMaxRow, "numMaxRow");
+            this.numMaxRow.Maximum = new decimal(new int[] {
+            60000,
+            0,
+            0,
+            0});
+            this.numMaxRow.Name = "numMaxRow";
+            this.numMaxRow.Value = new decimal(new int[] {
+            5000,
+            0,
+            0,
+            0});
+            // 
+            // lblSeperator
+            // 
+            resources.ApplyResources(this.lblSeperator, "lblSeperator");
+            this.lblSeperator.Name = "lblSeperator";
+            // 
+            // txtSeperator
+            // 
+            resources.ApplyResources(this.txtSeperator, "txtSeperator");
+            this.txtSeperator.Name = "txtSeperator";
+            // 
+            // progressConverting
+            // 
+            this.tableMain.SetColumnSpan(this.progressConverting, 2);
+            resources.ApplyResources(this.progressConverting, "progressConverting");
+            this.progressConverting.Name = "progressConverting";
             // 
             // openExcelFile
             // 
@@ -114,9 +202,13 @@
             // 
             // saveTargetFile
             // 
-            this.saveTargetFile.DefaultExt = "xlsx";
-            this.saveTargetFile.FileName = "yMKT_upload.xlsx";
+            this.saveTargetFile.DefaultExt = "csv";
+            this.saveTargetFile.FileName = "yMKT_upload.csv";
             resources.ApplyResources(this.saveTargetFile, "saveTargetFile");
+            // 
+            // timerProgress
+            // 
+            this.timerProgress.Tick += new System.EventHandler(this.progress_ticking);
             // 
             // frmMain
             // 
@@ -130,6 +222,10 @@
             this.tableMain.ResumeLayout(false);
             this.tableMain.PerformLayout();
             this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowParameters.ResumeLayout(false);
+            this.flowParameters.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numMaxCol)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMaxRow)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -140,13 +236,22 @@
         private System.Windows.Forms.TextBox txtOriginFileLocation;
         private System.Windows.Forms.Button btnRead;
         private System.Windows.Forms.TextBox txtTargetFileLocation;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnTargetFile;
         private System.Windows.Forms.RadioButton radioDMSbackend;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Button btnConvert;
         private System.Windows.Forms.Button btnCancel;
         private System.Windows.Forms.OpenFileDialog openExcelFile;
         private System.Windows.Forms.SaveFileDialog saveTargetFile;
+        private System.Windows.Forms.FlowLayoutPanel flowParameters;
+        private System.Windows.Forms.Label labelMaxCol;
+        private System.Windows.Forms.NumericUpDown numMaxCol;
+        private System.Windows.Forms.Label labelMaxRow;
+        private System.Windows.Forms.NumericUpDown numMaxRow;
+        private System.Windows.Forms.Label lblSeperator;
+        private System.Windows.Forms.TextBox txtSeperator;
+        private System.Windows.Forms.ProgressBar progressConverting;
+        private System.Windows.Forms.Timer timerProgress;
     }
 }
 
--- ConTool/ConTool/Form1.cs
+++ ConTool/ConTool/Form1.cs
@@ -16,11 +16,7 @@ namespace ConTool
         public frmMain()
         {
             InitializeComponent();
-        }
-
-        private void mainPanel_Paint(object sender, PaintEventArgs e)
-        {
-
+            btnConvert.Enabled = false;
         }
 
         private void btnRead_Click(object sender, EventArgs e)
@@ -36,6 +32,22 @@ namespace ConTool
                     {
                         txtOriginFileLocation.Text = openExcelFile.FileName;
                         stream.Close();
+                        var dirName = Path.GetDirectoryName(openExcelFile.FileName);
+                        var origin_fileName = Path.GetFileNameWithoutExtension(openExcelFile.FileName);
+                        var fileName = "yMKT_"+ origin_fileName + ".csv";
+                        var target = dirName + Path.DirectorySeparatorChar + fileName;
+                        var i = 0;
+                        while (File.Exists(target))
+                        {
+                            i++;
+                            fileName = "yMKT_" + origin_fileName + "(" + i + ").csv";
+                            target = dirName +Path.PathSeparator + fileName;
+                        }
+
+                        txtTargetFileLocation.Text = target;
+                        saveTargetFile.InitialDirectory = dirName;
+                        saveTargetFile.FileName = fileName;
+                        
                     }
                 }
                 catch (Exception ex)
@@ -43,6 +55,113 @@ namespace ConTool
                     MessageBox.Show("错误: 无法读取文件，原因：" + ex.Message);
                 }
             }
+        }
+
+        private void btnTargetFile_Click(object sender, EventArgs e)
+        {
+            if (saveTargetFile.ShowDialog() == DialogResult.OK)
+            {
+                txtTargetFileLocation.Text = saveTargetFile.FileName;
+            }
+        }
+
+        private void FileNames_changed(object sender, EventArgs e)
+        {
+            if (txtTargetFileLocation.Text.Length > 0 && txtOriginFileLocation.Text.Length > 0)
+            {
+                btnConvert.Enabled = true;
+            }
+            else
+            {
+                btnConvert.Enabled = false;
+            }
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            btnConvert.Enabled = false;
+            if (txtTargetFileLocation.Text.Length > 0 && txtOriginFileLocation.Text.Length > 0)
+            {
+                this.Cursor = Cursors.WaitCursor;
+                string result = this.Convert();
+                if (result.Length == 0)
+                {
+                    result = "转换成功！";
+                }
+                txtTargetFileLocation.Text = "";
+                txtOriginFileLocation.Text = "";
+                this.Cursor = Cursors.Default;
+                MessageBox.Show(result);
+            }
+            else
+            {
+                MessageBox.Show("请检查输入文件名和输出文件名，有错，谢谢。");
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <span class="code-SummaryComment"><summary></span>
+        /// Executes a shell command synchronously.
+        /// <span class="code-SummaryComment"></summary></span>
+        /// <span class="code-SummaryComment"><param name="command">string command</param></span>
+        /// <span class="code-SummaryComment"><returns>string, as output of the command.</returns></span>
+        private string ExecuteCommandSync(string command)
+        {
+            try 
+            {
+                // create the ProcessStartInfo using "cmd" as the program to be run,
+                // and "/c " as the parameters.
+                // Incidentally, /c tells cmd that we want it to execute the command that follows,
+                // and then exit.
+                System.Diagnostics.ProcessStartInfo procStartInfo =
+                    new System.Diagnostics.ProcessStartInfo("cmd", "/c " + command);
+
+                // The following commands are needed to redirect the standard output.
+                // This means that it will be redirected to the Process.StandardOutput StreamReader.
+                procStartInfo.RedirectStandardOutput = true;
+                procStartInfo.UseShellExecute = false;
+                // Do not create the black window.
+                procStartInfo.CreateNoWindow = true;
+                // Now we create a process, assign its ProcessStartInfo and start it
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                proc.StartInfo = procStartInfo;
+                proc.Start();
+                // Get the output into a string
+                string result = proc.StandardOutput.ReadToEnd();
+                // Display the command output.
+                return result;
+            }
+            catch (Exception objException)
+            {
+                // Log the exception
+                return objException.Message;
+            }
+        }
+
+        private string Convert()
+        {
+            string command = "converter";
+            command += " " + txtOriginFileLocation.Text;
+            command += " --output=" + txtTargetFileLocation.Text;
+            command += " --format=" + "1";
+            command += " --delimiter=" + txtSeperator.Text;
+            command += " --max_col=" + numMaxCol.Value;
+            command += " --total=" + numMaxRow.Value;
+            return this.ExecuteCommandSync(command);
+        }
+
+        private void progress_ticking(object sender, EventArgs e)
+        {
+            var tar = progressConverting.Value + 15;
+            if (tar >= progressConverting.Maximum)
+            {
+                tar = 0;
+            }
+            progressConverting.Value = tar;
         }
     }
 }
