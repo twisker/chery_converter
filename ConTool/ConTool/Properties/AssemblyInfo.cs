2edb7aa838229dfecaaecb481646b2871e7e01a7
wang ying <wang.ying@sap.com>
2018-04-02 23:24:13 +0800

    增加了IS_CONSUMER

--- ConTool/ConTool/Properties/AssemblyInfo.cs
+++ ConTool/ConTool/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ConTool")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("奇瑞SAP C4C/yMKT 线索转换小工具")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SAP SE")]
 [assembly: AssemblyProduct("ConTool")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.14")]
+[assembly: AssemblyFileVersion("0.1.0.14")]
